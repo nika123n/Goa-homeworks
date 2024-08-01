32ded935bc273bf3348de387257d37968ba7698d
Goa-homeworks <nikoloznikoloz234!gmail.com>
2024-08-01 14:58:24 +0400

    commit

--- day 45/homework/.py
+++ day 45/homework/.py
@@ -1,2 +1,8 @@
+#2) write an if-else statement that checks if a number is even or odd. if the number is even, print "even" otherwise,print"odd"
 
+number = 7  
 
+if number % 2 == 0:
+    print("even")
+else:
+    print("odd")
---
+++ day 49/homework/.py
@@ -0,0 +1,11 @@
+# 1)შექმენით სია რომელშიც იქნება 10 განსხვავებული რიცხვი შემდეგ კი დაბეჭდეთ ყველა ლისტის ელემენტის ჯამი
+numbers = [1,2,3,4,5,6,7,8,9,10]
+numbers_sum = sum(numbers)
+print(numbers_sum)
+
+# 7) შექმენით რიცხვებით სავსე სია, გამოითვალეთ ამ სიის ელემენტების საშუალო არითმეტიკული
+numbers = [1,2,3,4,5,6,7,8,9,10]
+sum_numbers = sum(numbers) 
+count = len(numbers)
+average = sum_numbers / count
+print(average)
